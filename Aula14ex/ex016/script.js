e11e935379c7c5bfa74240e500c689ae69d3cc7d
DevKauagm <kauag0988@gmail.com>
2025-09-11 22:48:47 -0300

    up

--- Aula14ex/ex016/script.js
+++ Aula14ex/ex016/script.js
@@ -1,15 +1,27 @@
+var saida = document.getElementById('saida')
 function contar() {
     var inicio = document.getElementById('inicio').value
     var fim = document.getElementById('fim').value
     var passo = document.getElementById('passo').value
-    var saida = document.getElementById('saida')
     if (inicio.length == 0 || fim.length == 0 || passo.length == 0) {
-        saida.innerHTML = 'Impossivel contar'
+        saida.innerText = 'Impossivel contar'
     } else {
-        var text = ''
-        for (inicio; inicio <= fim; passo) {
-            text += `${inicio}`
+        if (passo == 0) {
+            alert('não é possível ter passo 0, mas foi considerado como 1')
+            passo = 1
         }
-        saida.innerText = text
+        var text = ''
+        inicio = Number(inicio)
+        fim = Number(fim)
+        passo = Number(passo)
+        for (inicio; inicio < fim; inicio += passo) {
+            text += `${inicio}&#x1F449;`
+        }
+        if (passo >= fim) {
+            text += `${fim}&#x1F3F4;`
+        } else {
+            text += `${inicio}&#x1F3F4;`
+        }
+        saida.innerHTML = text
     }
 }
