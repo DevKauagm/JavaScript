6e2c40ff4b88eefe7087824c001585dbb566e4ee
DevKauagm <kauag0988@gmail.com>
2025-09-19 22:32:47 -0300

    Update script.js

--- Aula16ex/ex018/script.js
+++ Aula16ex/ex018/script.js
@@ -1,16 +1,18 @@
 var nums = []
 var saida = document.getElementById('saida')
 function adicionar() {
-    let num = document.getElementById('num').value
-    if (nums.indexOf(Number(num)) != -1 || num.length == 0 || Number(num) > 100 || Number(num) < 1) {
+    let num = document.getElementById('num')
+    if (nums.indexOf(Number(num.value)) != -1 || num.value.length == 0 || Number(num.value) > 100 || Number(num.value) < 1) {
         alert('[ERRO]-Não foi digitado nenhum número ou o número não está entre 1 e 100 ou o número já foi adicionado.')
     } else {
         saida.innerHTML = ''
-        nums.push(Number(num))
+        nums.push(Number(num.value))
         let op = document.createElement('option')
-        op.innerText = `Valor ${num} adicionado.`
+        op.innerText = `Valor ${num.value} adicionado.`
         document.getElementById('ad').appendChild(op)
     }
+    num.value = ''
+    num.focus()
 }
 
 function finalizar() {
