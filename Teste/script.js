5b16c029c5921880d1e541b148c89143ac8b9d4c
DevKauagm <kauag0988@gmail.com>
2025-10-04 21:46:51 -0300

    up

--- Teste/script.js
+++ Teste/script.js
@@ -1,11 +1,24 @@
 const cpf = document.getElementById('cpf')
+const tel = document.getElementById('tel')
+const data = document.getElementById('data')
+const output = document.querySelector('output')
 const regex = /\d{3}[.]?\d{3}[.]?\d{3}[-]?\d{2}/
+let text = 'telefones encontrados: '
 
 function resultado() {
-    console.log(regex.test(cpf.value))
+    output.innerText = ''
     if (regex.test(cpf.value)) {
-        alert('Formato válido')
+        cpf.value = 'cpf enviado com sucesso'
     } else {
-        alert('Formato inválido')
+        cpf.value = 'erro digite novamente'
+        cpf.focus()
     }
+    setTimeout(() => {cpf.value=''}, 1000)
+    const tels = tel.value.match(/\d{2}[-]?\d{4,5}[-]?\d{4}/g)
+    for (const i in tels) {
+        text += `${tels[i]} `
+    }
+    output.innerText = text
+    console.log(/(\d{4})[-]?(\d{1,2})[-]?(\d{1,2})/.exec(data.value))
+    data.value = data.value.replace(/(\d{4})[-]?(\d{1,2})[-]?(\d{1,2})/, /$3-$2-$1/)
 }
